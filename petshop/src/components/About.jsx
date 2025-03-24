2ee3d3d2c9b08d94088fe388e4907b5e70aeed56
acaf02 <anacarol.alves1@outlook.com>
2025-03-24 20:36:21 -0300

    style: centralize the title

--- petshop/src/components/About.jsx
+++ petshop/src/components/About.jsx
@@ -5,30 +5,32 @@ import Title from './Title';
 
 const About = () => {
     return (
-        <section className="w-full px-10 md:px-28 py-10 flex flex-col md:flex-row items-center justify-center gap-8">
-            <div className="w-full md:w-1/3">
-                <div className="md:pb-4">
-                    <Title text="Sobre mim" />
-
-                </div>
-                <img
-                    src={assets.about}
-                    alt="Ambiente decorado"
-                    className="rounded-xl w-full"
-                />
+        <section className="w-full px-10 md:px-32 py-10 flex flex-col items-center">
+            <div className="w-full text-center mb-6 py-4">
+                <Title text="Sobre Nós" />
             </div>
 
-            <div className="w-full md:w-2/3 flex flex-col items-start md:pl-8">
-                <p className="text-base text-gray-700 md:pt-28">
-                    Na Lumus Solem Petshop, acreditamos que cuidar de um pet vai muito além de oferecer produtos e serviços — é sobre amor, confiança e bem-estar. Nossa loja nasceu do carinho e da dedicação aos animais, com o propósito de oferecer tudo o que seu pet precisa para viver com saúde, alegria e qualidade de vida.
-                </p>
+            <div className="w-full flex flex-col md:flex-row items-center gap-8 pb-7">
+                <div className="w-full md:w-1/3">
+                    <img
+                        src={assets.about}
+                        alt="Ambiente decorado"
+                        className="rounded-xl w-full"
+                    />
+                </div>
 
-                <div className="py-10">
-                    <Link to="/about">
-                        <button className="bg-black text-white font-medium py-2 px-12 rounded-4xl hover:bg-purple-700 transition">
-                            SAIBA MAIS
-                        </button>
-                    </Link>
+                <div className="w-full md:w-2/3 flex flex-col items-start">
+                    <p className="text-base text-gray-700">
+                        Na Lumus Solem Petshop, acreditamos que cuidar de um pet vai muito além de oferecer produtos e serviços — é sobre amor, confiança e bem-estar. Nossa loja nasceu do carinho e da dedicação aos animais, com o propósito de oferecer tudo o que seu pet precisa para viver com saúde, alegria e qualidade de vida.
+                    </p>
+
+                    <div className="py-6">
+                        <Link to="/about">
+                            <button className="bg-black text-white font-medium py-2 px-6 md:px-12 rounded-4xl hover:bg-purple-700 transition">
+                                SAIBA MAIS
+                            </button>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </section>
