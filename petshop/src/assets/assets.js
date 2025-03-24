41a8174ed88f151401ec5bd8e30c97c7f0f52562
acaf02 <anacarol.alves1@outlook.com>
2025-03-24 20:35:30 -0300

    feat: add new photos and its importation to the project

--- petshop/src/assets/assets.js
+++ petshop/src/assets/assets.js
@@ -1,7 +1,22 @@
 import hero from "./hero.mp4"
 import about from "./about.jpg"
+import banho from "./banho-tosa.png"
+import hospedagem from "./hospedagem.png"
+import dogwalker from "./dogwalker.png"
+import petfood from "./pet-food.png"
+import service from "./service.png"
+import transporte from "./transporte.png"
+import veterinario from "./veterinario.png"
+
 
 export const assets = {
     hero,
-    about
+    about,
+    banho,
+    hospedagem,
+    dogwalker,
+    petfood,
+    service,
+    transporte,
+    veterinario
     }
