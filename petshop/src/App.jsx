d5ac3d5e44538cfe352e02789ec90da0d374ac64
acaf02 <anacarol.alves1@outlook.com>
2025-03-26 17:57:04 -0300

    cleanup: remove the navigation importation

--- petshop/src/App.jsx
+++ petshop/src/App.jsx
@@ -4,14 +4,12 @@ import Service from './pages/Service';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import About from './pages/About';
-import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <Router>
       <div className="w-full">
-        <Navigation />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
