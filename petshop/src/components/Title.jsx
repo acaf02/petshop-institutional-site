6674eac715fde9d7bc0a374ed44c6e5441a7bc02
acaf02 <anacarol.alves1@outlook.com>
2025-03-30 15:42:56 -0300

    style: make the text smaller

--- petshop/src/components/Title.jsx
+++ petshop/src/components/Title.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Title = ({ text }) => {
   return (
-    <h2 className="text-6xl font-bold border-b-2 border-purple-600 inline-block mb-4">
+    <h2 className="text-5xl font-bold border-b-2 border-purple-600 inline-block mb-4">
       {text}
     </h2>
   );
